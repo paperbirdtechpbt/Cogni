fe8211c1f9c0cdea32c6e840eb3d433139acee9d
DESKTOP-UF26GCM\lenovo <paperbirdtech@gmail.com>
2021-09-30 10:22:37 +0530

    Firebase Notification Added

---
+++ app/src/main/java/com/pbt/cogni/fcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,104 @@
+package com.pbt.cogni.fcm
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
+import com.pbt.cogni.R
+import com.pbt.cogni.activity.chat.ChatActivity
+import org.json.JSONObject
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+
+
+              var obj : JSONObject = JSONObject(remoteMessage.data.toString())
+
+
+//            Log.d(TAG, "Message  payload: "+ Gson().toJson(obj))
+            Log.d(TAG, "Message  obj: "+ obj.getString("payload"))
+
+
+
+//            if (/* Check if data needs to be processed by long running job */ true) {
+//                // For long-running tasks (10 seconds or more) use WorkManager.
+//                scheduleJob()
+//            } else {
+//                // Handle message within 10 seconds
+//                handleNow()
+//            }
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+//            sendNotification(it.body.ge)
+        }
+
+    }
+
+    //new Token genrated
+    override fun onNewToken(token: String) {
+        sendRegistrationToServer(token)
+    }
+
+    //registion token  send
+    private fun sendRegistrationToServer(token: String?) {
+        Log.d(TAG, "sendRegistrationTokenToServer($token)")
+    }
+
+    //show notification
+    private fun sendNotification(messageBody: String) {
+        val intent = Intent(this, ChatActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic__chat_profile)
+            .setContentTitle(messageBody)
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
+    }
+}
