7053d6a79d065e0ec1bae5890044bed997c9dea9
ravidabhi32 <90024072+ravidabhi32@users.noreply.github.com>
2021-11-03 13:15:14 +0530

    Get path and URI of image in android 11

--- app/src/main/java/com/pbt/cogni/activity/Test/ImageUploadUri.kt
+++ app/src/main/java/com/pbt/cogni/activity/Test/ImageUploadUri.kt
@@ -1,6 +1,7 @@
 package com.pbt.cogni.activity.Test
 
 
+import android.R.attr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pbt.cogni.R
@@ -13,13 +14,30 @@ import java.io.File
 import android.graphics.Bitmap
 import android.app.AlertDialog
 import android.content.Context
+import android.graphics.BitmapFactory
+import android.os.Environment
 import android.provider.MediaStore.Images
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.pbt.cogni.BuildConfig
 import java.io.ByteArrayOutputStream
+import android.provider.OpenableColumns
+
+import android.R.attr.data
+import android.app.Activity
+import android.database.Cursor
 
 
 class ImageUploadUri : AppCompatActivity() {
 
     private val REQUEST_CAMERA_IMAGE = 10
+
+    val APP_TAG = "##ImageUploadUri"
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034
+    val GET_DOCUMENT=1100
+    val photoFileName = "photo.jpg"
+    var photoFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,60 +54,151 @@ class ImageUploadUri : AppCompatActivity() {
 //
 //        }
 //hgh
+//        imageuri.setOnClickListener{
+//
+//            val m_intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//            startActivityForResult(m_intent, REQUEST_CAMERA_IMAGE)
+//        }
         imageuri.setOnClickListener{
-            openDialog()
-            val m_intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(m_intent, REQUEST_CAMERA_IMAGE)
+            onLaunchCamera()
         }
 
     }
 
-    private fun openDialog() {
-        val myAlertDialog: AlertDialog.Builder = AlertDialog.Builder(
-            this
-        )
-        myAlertDialog.setTitle("Upload Pictures Option")
-        myAlertDialog.setMessage("How do you want to set your picture?")
+    private fun onLaunchCamera() {
+//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val intent = Intent()
+        intent.setAction(Intent.ACTION_GET_CONTENT)
+        val i = intent.setType("*/*").toString()
 
+        if (i == "*/*") {
+             startActivityForResult(intent,GET_DOCUMENT)
+        } else{
+
+
+            photoFile = getPhotoFileUri(photoFileName)
+        Log.d(APP_TAG, photoFileName)
+        if (photoFile != null) {
+            val fileProvider: Uri =
+                FileProvider.getUriForFile(
+                    this,
+                    BuildConfig.APPLICATION_ID + ".provider",
+                    photoFile!!
+                )
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+
+            Log.d(APP_TAG, fileProvider.toString())
+
+
+            if (intent.resolveActivity(packageManager) != null) {
+
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
+            }
+        }
+    }
     }
 
+    private fun getPhotoFileUri(fileName: String): File{
+        val mediaStorageDir =
+            File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG)
+
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(APP_TAG, "failed to create directory")
+        }
+
+        return File(mediaStorageDir.path + File.separator + fileName)
+    }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-            REQUEST_CAMERA_IMAGE -> if (resultCode == RESULT_OK) {
-                val photo :Bitmap= data?.getExtras()?.get("data")as Bitmap
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
 
-                imageviewimage.setImageBitmap(photo)
-                val tempUri: Uri = getImageUri(applicationContext, photo)
+                val takenImage = BitmapFactory.decodeFile(photoFile!!.absolutePath)
+                val ivPreview: ImageView = findViewById(R.id.imageviewimage)
+                ivPreview.setImageBitmap(takenImage)
+            } else {
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else{
+            if (resultCode === RESULT_OK) {
+                // Get the Uri of the selected file
+                val uri: Uri = data?.getData()!!
+                val uriString = uri.toString()
+                val myFile = File(uriString)
+                val path = myFile.absolutePath
+                var displayName: String? = null
+                if (uriString.startsWith("content://")) {
+                    var cursor: Cursor? = null
+                    try {
+                        cursor =
+                            this.getContentResolver().query(uri, null, null, null, null)
+                        if (cursor != null && cursor.moveToFirst()) {
+                            displayName =
+                                cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                            Log.d(APP_TAG,"myfile"+myFile.toString()+"\n"+"Uri"+uriString)
+                            Toast.makeText(this,displayName,Toast.LENGTH_LONG).show()
+                        }
+                    } finally {
+                        cursor?.close()
+                    }
+                } else if (uriString.startsWith("file://")) {
+                    displayName = myFile.name
+                    Log.d(APP_TAG,"myfile"+myFile.toString()+"\n"+"Uri"+uriString)
+                    Toast.makeText(this,displayName,Toast.LENGTH_LONG).show()
 
-                val finalFile = File(getRealPathFromURIII(tempUri))
-
-                Log.d("####pathandFILE",tempUri.toString()+"\n"+finalFile.toString())
+                }
             }
         }
     }
+//
+//    private fun openDialog() {
+//        val myAlertDialog: AlertDialog.Builder = AlertDialog.Builder(
+//            this
+//        )
+//        myAlertDialog.setTitle("Upload Pictures Option")
+//        myAlertDialog.setMessage("How do you want to set your picture?")
+//
+//    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        when (requestCode) {
+//            REQUEST_CAMERA_IMAGE -> if (resultCode == RESULT_OK) {
+//                val photo :Bitmap= data?.getExtras()?.get("data")as Bitmap
+//
+//                imageviewimage.setImageBitmap(photo)
+//                val tempUri: Uri = getImageUri(applicationContext, photo)
+//
+//                val finalFile = File(getRealPathFromURIII(tempUri))
+//                Toast.makeText(this,tempUri.toString()+"\n"+finalFile.toString(),Toast.LENGTH_LONG).show()
+//
+//                Log.d("####pathandFILE",tempUri.toString()+"\n"+finalFile.toString())
+//            }
+//        }
+//    }
 
 
-    fun getRealPathFromURIII(uri: Uri?): String {
-        var path = ""
-        if (contentResolver != null) {
-            val cursor = contentResolver.query(uri!!, null, null, null, null)
-            if (cursor != null) {
-                cursor.moveToFirst()
-                val idx = cursor.getColumnIndex(Images.ImageColumns.DATA)
-                path = cursor.getString(idx)
-                cursor.close()
-            }
-        }
-        return path
-    }
-
-    fun getImageUri(inContext: Context, inImage: Bitmap): Uri{
-        val bytes = ByteArrayOutputStream()
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-        val path = Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null)
-        return Uri.parse(path)
-    }
+//    fun getRealPathFromURIII(uri: Uri?): String {
+//        var path = ""
+//        if (contentResolver != null) {
+//            val cursor = contentResolver.query(uri!!, null, null, null, null)
+//            if (cursor != null) {
+//                cursor.moveToFirst()
+//                val idx = cursor.getColumnIndex(Images.ImageColumns.DATA)
+//                path = cursor.getString(idx)
+//                cursor.close()
+//            }
+//        }
+//        return path
+//    }
+//
+//    fun getImageUri(inContext: Context, inImage: Bitmap): Uri{
+//        val bytes = ByteArrayOutputStream()
+//        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+//        val path = Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null)
+//        return Uri.parse(path)
+//    }
 
     //    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 //        super.onActivityResult(requestCode, resultCode, data)
